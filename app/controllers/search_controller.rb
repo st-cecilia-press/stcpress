45568c5d083458d277972a4f5a56965e74fecea9
Monique Rio <mrio@umich.edu>
2016-09-17 08:24:46 -0400

    more stuff working; tests still not

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,14 @@
+require 'open-uri'
+class SearchController < ApplicationController
+  def create
+    slug = params[:slug]
+    search = params[:search]
+    if slug.blank?
+        search = URI::encode(search)
+        url = "https://cse.google.com/cse?as_q=#{search}&client=google-csbe&cx=005631129422479805654:q04zkci2lz4"
+        redirect_to(url)
+    else
+        redirect_to piece_url(:slug => slug)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
     get 'metadata_generator' => 'metadata_generator#new'
   end
   
-  resources :home, only: [:create]
+  resources :search, only: [:create]
   root 'home#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

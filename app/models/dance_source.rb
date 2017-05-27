7d89f28137b8d3d5d69f3bb88730bb7bf8669928
Monique Rio <nique.rio@gmail.com>
2017-05-27 14:00:45 -0400

    one more dependent destroy

--- app/models/dance_source.rb
+++ app/models/dance_source.rb
@@ -1,4 +1,4 @@
 class DanceSource < ActiveRecord::Base
-  has_many :dance_source_contents
+  has_many :dance_source_contents, dependent: :destroy
   has_many :dances, through: :dance_source_contents
 end

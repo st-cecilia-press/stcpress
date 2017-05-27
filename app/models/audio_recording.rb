921f3f3d928ebee8048eb90e4e1dd1de7b3a1864
Monique Rio <nique.rio@gmail.com>
2017-05-27 14:22:00 -0400

    more the same

--- app/models/audio_recording.rb
+++ app/models/audio_recording.rb
@@ -1,5 +1,5 @@
 class AudioRecording < ActiveRecord::Base
   belongs_to :ensemble
-  has_many :instruction_audios 
+  has_many :instruction_audios, dependent: :destroy
   has_many :instructions, through: :instruction_audios
 end

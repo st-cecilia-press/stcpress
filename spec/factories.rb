aaa389c7a9ee9704374f41dfc0c929f0509fa129
Monique Rio <mrio@umich.edu>
2020-04-15 09:16:43 -0400

    updated to rails 5.2

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,20 +1,20 @@
 FactoryBot.define do
   factory :page do
-    slug "MyString"
-    title "MyString"
-    body "MyText"
+    slug { "MyString" }
+    title { "MyString" }
+    body { "MyText" }
   end
   sequence :email do |n|
-    "person#{n}@example.com"
+     "person#{n}@example.com"
   end
   factory :user do
     email {generate :email}
-    password Devise.friendly_token.first(8)  
-    name 'Mundungus'
-    admin false
+    password { Devise.friendly_token.first(8) }
+    name { "Mundungus" }
+    admin { false }
   end
   factory :audio_recording do
-    slug 'audio' 
+    slug { "audio" } 
     ensemble
   end
   factory :instruction_audio do
@@ -30,66 +30,66 @@ FactoryBot.define do
     dance_source 
   end
   factory :dance_facsimile_image do
-    url "MyString"
-    filename "MyString"
-    description "MyString"
-    name "MyString"
+    url { "MyString" }
+    filename { "MyString" }
+    description { "MyString" }
+    name { "MyString" }
   end
   factory :dance_translation do
-    name "MyString"
-    url "MyString"
+    name { "MyString" }
+    url { "MyString" }
   end
   factory :dance_source do
-    start_date 1651 
-    end_date 1651
-    title "Earliest Dance Source"
-    slug "earliest_dance_source"
+    start_date { 1651 } 
+    end_date { 1651 }
+    title { "Earliest Dance Source" }
+    slug { "earliest_dance_source" }
   end
   factory :music_file do
-    filename "MyString"
-    source "MyString"
-    name "MyString"
+    filename { "MyString" }
+    source { "MyString" }
+    name { "MyString" }
     sheet_music nil
   end
   factory :sheet_music do
     
   end
   factory :video_recording do
-    name "MyString"
-    youtube "MyString"
+    name { "MyString" }
+    youtube { "MyString" }
   end
   factory :audioRecording do
     ensemble 
-    purchase_url "MyString"
-    youtube "MyString"
-    file "MyString"
+    purchase_url { "MyString" }
+    youtube { "MyString" }
+    file { "MyString" }
   end
   factory :ensemble do
-    description "MyString"
-    name "MyString"
-    slug "MyString"
+    description { "MyString" }
+    name { "MyString" }
+    slug { "MyString" }
   end
   factory :instruction_type do
-    name "MyString"
+    name { "MyString" }
   end
   factory :instruction do
-    text "MyText"
+    text { "MyText" }
     dance 
     instruction_type
   end
   factory :person do
-    name "MyString"
+    name { "MyString" }
   end
   factory :dance_category do
-    name "English Country"
+    name { "English Country" }
   end
   factory :dance do
-    title "Gathering Peascods"
-    slug "gathering_peascods"
+    title { "Gathering Peascods" }
+    slug { "gathering_peascods" }
   end
   factory :facsimile_source do
-    name "MyString"
-    url "MyString"
+    name { "MyString" }
+    url { "MyString" }
     manuscript 
     book 
   end
@@ -102,50 +102,50 @@ FactoryBot.define do
     piece
   end
   factory :language do
-    name "English"
+    name { "English" }
   end
   factory :tag do
-    name "frottola"
+    name { "frottola" }
   end
   factory :publicationship do
     piece 
     repository 
   end
   factory :translations_collection do
-    name "Repo"
-    slug "repo"
+    name { "Repo" }
+    slug { "repo" }
   end
   factory :instrumental_book do
-    name "Repo"
-    slug "repo"
+    name { "Repo" }
+    slug { "repo" }
   end
   factory :vocal_collection do
-    name "Repo"
-    slug "repo"
+    name { "Repo" }
+    slug { "repo" }
   end
   factory :repository do
-    name "My Repo"
-    slug "my_repo"
+    name { "My Repo" }
+    slug { "my_repo" }
   end
   factory :song_voicing do
     voicing 
     piece 
   end
   factory :voicing do
-    name "SSSSSSSSSSSB"
+    name { "SSSSSSSSSSSB" }
   end
   factory :image do
     book_content
     manuscript_content
-    name "Image Name" 
+    name { "Image Name" } 
   end
   factory :book_content do
     book 
     piece 
   end
   factory :book do
-    title "Biggest Book of Chansons"
-    date Date.new(1600)
+    title { "Biggest Book of Chansons" }
+    date { Date.new(1600) }
     sequence(:slug) { |n| "slug#{n}" }
   end
   factory :manuscript_content do
@@ -153,15 +153,15 @@ FactoryBot.define do
     piece 
   end
   factory :manuscript do
-    name "Blackcloak Manuscript"
+    name { "Blackcloak Manuscript" }
     sequence(:slug) { |n| "slug#{n}" }
   end
   factory :piece do
-    title "My Piece"
+    title { "My Piece" }
     composer
-    slug "slug"
+    slug { "slug" }
   end
   factory :composer do
-    name "Josquin"
+    name { "Josquin" }
   end
 end
